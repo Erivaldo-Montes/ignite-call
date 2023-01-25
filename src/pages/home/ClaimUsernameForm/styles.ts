74289e6473c251ef69a496f34fc79f3b253d95cd
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-01-25 09:56:23 -0300

    core: hero home page

---
+++ src/pages/home/ClaimUsernameForm/styles.ts
@@ -0,0 +1,9 @@
+import { Box, styled } from '@ignite-ui/react'
+
+export const Form = styled(Box, {
+  display: 'grid',
+  gridTemplateColumns: '1fr auto',
+  gap: '$2',
+  marginTop: '$4',
+  padding: '$4',
+})
