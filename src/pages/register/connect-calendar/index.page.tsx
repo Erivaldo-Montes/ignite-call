108a6ef5bc0420c9da42f78315ee22464e148ac1
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-01-28 22:36:09 -0300

    auth google

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,8 +1,15 @@
-import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { signIn } from 'next-auth/react'
 import { ArrowRight } from 'phosphor-react'
+// import { api } from "../../../lib/axios"
 import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
 
 export default function Register() {
+  // async function handleRegister() {
+
+  // }
+
   return (
     <Container>
       <Header>
@@ -11,13 +18,28 @@ export default function Register() {
           Conecte o seu calendário para verificar automaticamente as horas
           ocupadas e os novos eventos à medida em que são agendados.
         </Text>
+
         <MultiStep size={4} currentStep={2} />
       </Header>
 
-      <Button type="submit" disabled={isSubmitting}>
-        Próximo passo
-        <ArrowRight />
-      </Button>
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
+            Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
     </Container>
   )
 }
--- src/pages/register/connect-calendar/styles.ts
+++ src/pages/register/connect-calendar/styles.ts
@@ -10,4 +10,10 @@ export const ConnectItem = styled('div', {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
+
+  border: '1px solid $gray600',
+  padding: '$4 $6',
+  borderRadius: '$md',
+
+  marginBottom: '$2',
 })
