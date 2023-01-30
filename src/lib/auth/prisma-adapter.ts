453f657c35b42fcd77a371b767bea99d3c39c34b
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-01-30 19:00:20 -0300

    auth

---
+++ src/lib/auth/prisma-adapter.ts
@@ -0,0 +1,228 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Adapter } from 'next-auth/adapters'
+import { prisma } from '../prisma'
+import { parseCookies, destroyCookie } from 'nookies'
+
+// prisma adpter personalizado
+export function PrismaAdapter(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Adapter {
+  return {
+    async createUser(user) {
+      const { '@ignitecall:userId': userIdOnCookies } = parseCookies({ req })
+
+      if (!userIdOnCookies) {
+        throw new Error('user ID not found on cookies')
+      }
+
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: userIdOnCookies,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      })
+
+      destroyCookie({ res }, '@ignitecall:userId', {
+        path: '/',
+      })
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        avatar_url: prismaUser.avatar_url!,
+        email: prismaUser.email!,
+        emailVerified: null,
+      }
+    },
+
+    async getUser(id) {
+      const prismaUser = await prisma.user.findUnique({
+        where: {
+          id,
+        },
+      })
+
+      if (!prismaUser) {
+        return null
+      }
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        avatar_url: prismaUser.avatar_url!,
+        email: prismaUser.email!,
+        emailVerified: null,
+      }
+    },
+    async getUserByEmail(email) {
+      const user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!user) {
+        return null
+      }
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        avatar_url: user.avatar_url!,
+        email: user.email!,
+        emailVerified: null,
+      }
+    },
+
+    async getUserByAccount({ providerAccountId, provider }) {
+      const account = await prisma.account.findUnique({
+        where: {
+          provider_provider_account_id: {
+            provider,
+            provider_account_id: providerAccountId,
+          },
+        },
+        include: {
+          user: true,
+        },
+      })
+
+      if (!account) {
+        return null
+      }
+
+      const { user } = account
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        avatar_url: user.avatar_url!,
+        email: user.email!,
+        emailVerified: null,
+      }
+    },
+
+    async updateUser(user) {
+      const updatedUser = await prisma.user.update({
+        where: {
+          id: user.id!,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      })
+      return {
+        id: updatedUser.id,
+        name: updatedUser.name,
+        username: updatedUser.username,
+        avatar_url: updatedUser.avatar_url!,
+        email: updatedUser.email!,
+        emailVerified: null,
+      }
+    },
+
+    async linkAccount(account) {
+      await prisma.account.create({
+        data: {
+          user_id: account.userId,
+          type: account.type,
+
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+
+          refresh_token: account.refresh_token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+
+          scope: account.scope,
+          id_token: account.id_token,
+          session_state: account.session_state,
+        },
+      })
+    },
+    async unlinkAccount({ providerAccountId, provider }) {},
+
+    async createSession({ sessionToken, userId, expires }) {
+      await prisma.session.create({
+        data: {
+          session_token: sessionToken,
+          user_id: userId,
+          expires,
+        },
+      })
+
+      return {
+        sessionToken,
+        userId,
+        expires,
+      }
+    },
+    async getSessionAndUser(sessionToken) {
+      const prismaSession = await prisma.session.findUnique({
+        where: {
+          session_token: sessionToken,
+        },
+        include: {
+          user: true,
+        },
+      })
+
+      if (!prismaSession) {
+        return null
+      }
+
+      const { user, ...session } = prismaSession
+
+      return {
+        session: {
+          expires: session.expires,
+          sessionToken: session.session_token,
+          userId: session.user_id,
+        },
+        user: {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          avatar_url: user.avatar_url!,
+          email: user.email!,
+          emailVerified: null,
+        },
+      }
+    },
+    async updateSession({ sessionToken, expires, userId }) {
+      const prismaSession = await prisma.session.update({
+        where: {
+          session_token: sessionToken,
+        },
+        data: {
+          session_token: sessionToken,
+          expires,
+          user_id: userId,
+        },
+      })
+      return {
+        sessionToken: prismaSession.session_token,
+        userId: prismaSession.user_id,
+        expires: prismaSession.expires,
+      }
+    },
+    async deleteSession(sessionToken) {
+      await prisma.session.delete({
+        where: {
+          session_token: sessionToken,
+        },
+      })
+    },
+  }
+}
--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,32 +1,55 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import NextAuth, { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
+import { PrismaAdapter } from 'src/lib/auth/prisma-adapter'
 
-export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRETE ?? '',
-      authorization: {
-        params: {
-          scope:
-            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar ',
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): NextAuthOptions {
+  return {
+    // adpter personalizado
+    adapter: PrismaAdapter(req, res),
+
+    // Configure one or more authentication providers
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRETE ?? '',
+        authorization: {
+          params: {
+            scope:
+              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar ',
+          },
         },
+        profile(profile: GoogleProfile) {
+          return {
+            id: profile.sub,
+            email: profile.email,
+            name: profile.name,
+            username: '',
+            avatar_url: profile.picture,
+          }
+        },
+      }),
+      // ...add more providers here
+    ],
+
+    callbacks: {
+      async signIn({ account }) {
+        if (
+          !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+        ) {
+          return '/register/connect-calendar/?error=permissions'
+        }
+
+        return true
       },
-    }),
-    // ...add more providers here
-  ],
-
-  callbacks: {
-    async signIn({ account }) {
-      if (
-        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
-      ) {
-        return '/register/connect-calendar/?error=permissions'
-      }
-
-      return true
     },
-  },
+  }
 }
-export default NextAuth(authOptions)
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  // Do whatever you want here, before the request is passed down to `NextAuth`
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}
