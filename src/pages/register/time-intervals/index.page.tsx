4dd62751b5e17164f8faaf47f72148afb08981eb
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-02-02 21:25:03 -0300

    intervals time forms

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -7,6 +7,9 @@ import {
   TextInput,
 } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { Controller, useFieldArray, useForm } from 'react-hook-form'
+import { getWeekDays } from 'src/utils/get-week-day'
+import { z } from 'zod'
 // import { api } from "../../../lib/axios"
 import { Container, Header } from '../styles'
 import {
@@ -18,6 +21,35 @@ import {
 } from './styles'
 
 export default function TimeIntervals() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 1, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 2, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 3, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 4, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 5, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 6, enabled: false, startTime: '08:00', endTime: '18:00' },
+      ],
+    },
+  })
+
+  // cria um array interavél com os valores de um campo do hook-form
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  })
+
+  const weekDays = getWeekDays()
+
+  const intervals = watch('intervals')
   return (
     <Container>
       <Header>
@@ -30,28 +62,48 @@ export default function TimeIntervals() {
         <MultiStep size={4} currentStep={2} />
       </Header>
 
-      <IntervalBox>
-        <IntervalsContainer as="form">
-          <IntervalItem>
+      <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeInterval)}>
+        <IntervalsContainer>
+          {fields.map((field, index) => {
+            return (
+              <IntervalItem key={field.id}>
             <IntervalDay>
-              <Checkbox />
-              <Text>Segunda-feira</Text>
+                  {/* permite que elementos não nativos do html altere campos do formulário */}
+                  <Controller
+                    name={`intervals.${index}.enabled`}
+                    control={control}
+                    render={({ field }) => {
+                      return (
+                        <Checkbox
+                          onCheckedChange={(checked) => {
+                            field.onChange(checked === true)
+                          }}
+                          checked={field.value}
+                        />
+                      )
+                    }}
+                  />
+                  <Text>{weekDays[field.weekDay]}</Text>
             </IntervalDay>
             <IntervalInputs>
-              <TextInput size={'sm'} type="time" step={60} />
-              <TextInput size={'sm'} type="time" step={60} />
+                  <TextInput
+                    size={'sm'}
+                    type="time"
+                    step={60}
+                    disabled={intervals[index].enabled === false}
+                    {...register(`intervals.${index}.startTime`)}
+                  />
+                  <TextInput
+                    size={'sm'}
+                    type="time"
+                    step={60}
+                    disabled={intervals[index].enabled === false}
+                    {...register(`intervals.${index}.endTime`)}
+                  />
             </IntervalInputs>
           </IntervalItem>
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>terça-feira</Text>
-            </IntervalDay>
-            <IntervalInputs>
-              <TextInput size={'sm'} type="time" step={60} />
-              <TextInput size={'sm'} type="time" step={60} />
-            </IntervalInputs>
-          </IntervalItem>
+            )
+          })}
         </IntervalsContainer>
         <Button>
           proximo
