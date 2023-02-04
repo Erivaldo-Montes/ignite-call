11b0343ba217a657dfc387aba5e093d197a8242c
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-02-04 20:28:43 -0300

    create time intervals in DB

---
+++ src/pages/api/users/time-intervals.api.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+import { prisma } from 'src/lib/prisma'
+import { z } from 'zod'
+import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+
+const timeIntervalBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals } = timeIntervalBodySchema.parse(req.body)
+
+  // devido a limitação do sqlite de criar multiplos refistros devemos criar um registro por vez
+  await Promise.all(
+    intervals.map(async (interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          time_end_in_minutes: interval.endTimeInMinutes,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          week_day: interval.weekDay,
+          user_id: session.user.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
+}
