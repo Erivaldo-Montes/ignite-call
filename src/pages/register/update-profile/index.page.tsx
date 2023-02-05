64dc01cc58255b6a781c9379ab8308c58baebe10
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-02-05 10:09:02 -0300

    update profile page

---
+++ src/pages/register/update-profile/index.page.tsx
@@ -0,0 +1,80 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Heading, MultiStep, Text, TextArea } from '@ignite-ui/react'
+import { GetServerSideProps } from 'next'
+import { getServerSession } from 'next-auth'
+import { useSession } from 'next-auth/react'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { buildNextAuthOptions } from 'src/pages/api/auth/[...nextauth].api'
+import { z } from 'zod'
+import { Container, Header } from '../styles'
+import { FormAnnotation, ProfileBox } from './styles'
+
+const registerFormSchema = z.object({
+  bio: z.string(),
+})
+
+type updateProfileFormSchema = z.infer<typeof registerFormSchema>
+
+export default function UdateProfile() {
+  const {
+    register,
+    handleSubmit,
+
+    formState: { isSubmitting },
+  } = useForm<updateProfileFormSchema>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  const session = useSession()
+
+  console.log(session)
+
+  async function handleUpdateProfile(data: updateProfileFormSchema) {}
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+
+      <ProfileBox as={'form'} onSubmit={handleSubmit(handleUpdateProfile)}>
+        <label>
+          <Text size={'sm'}>Foto de perfil</Text>
+        </label>
+
+        <label>
+          <Text size="sm">Sobre você</Text>
+          <TextArea {...register('bio')} />
+          <FormAnnotation size={'sm'}>
+            Fale um pouco sobre você. Isto será exibido em sua página pessoal.
+          </FormAnnotation>
+        </label>
+
+        <Button type="submit" disabled={isSubmitting}>
+          Finalizar
+          <ArrowRight />
+        </Button>
+      </ProfileBox>
+    </Container>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  return {
+    props: {
+      session,
+    },
+  }
+}
